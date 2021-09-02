413b74bab19ec495a3dfffefe80ed2e67fee7bc5
Matheus Souza <matheussouza@pop-os.localdomain>
2021-09-02 07:13:25 -0300

    Header

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,9 +1,37 @@
 import React from "react";
-
-import { Container } from "./Dashboard.styles";
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGretting,
+  UserName,
+  UserWrapper,
+  PowerIcon,
+} from "./Dashboard.styles";
 
 const Dashboard = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://static.vecteezy.com/system/resources/thumbnails/000/439/863/small/Basic_Ui__28186_29.jpg",
+              }}
+            />
+            <User>
+              <UserGretting>Olá,</UserGretting>
+              <UserName>Matheus</UserName>
+            </User>
+          </UserInfo>
+          <PowerIcon />
+        </UserWrapper>
+      </Header>
+    </Container>
+  );
 };
 
 export default Dashboard;
